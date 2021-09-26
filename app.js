a115c43fabcf1e5e892141519c5733dcf8ad7ab5
Hibram007 <87105978+Hibram007@users.noreply.github.com>
2021-09-26 09:39:21 -0700

    inquierer prompt and data saving array added

--- app.js
+++ app.js
@@ -1,22 +1,92 @@
 const inquirer = require('inquirer');
 
-inquirer
-    .prompt([
+// an encapusulating function where "return" runs the inquierer prompt
+const promptUser = () => {
+    return inquirer.prompt([
+        //profile questions
         {
             type:'input',
             name: 'name',
             message: 'What is your name?'
-        }
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: 'Enter your GitHub Username'
+          },
+          {
+            type: 'input',
+            name: 'about',
+            message: 'Provide some information about yourself:'
+          }
+    ]);
+};
+
+// portfilio Data is the argument being accepted
+const promptProject = portfolioData => {
+    // empty array is essentially the data collecting system.
+  // If there's no 'projects' array property, create one --- this is a check to make sure array is not cleared each time funct is called
+if (!portfolioData.projects) {
+  portfolioData.projects = [];
+}
+    console.log(`
+  =================
+  Add a New Project
+  =================
+  `);
+  return inquirer.prompt([
+      {
+          type: 'input', 
+          name: 'description',
+          mesage: 'Provide a description of the project (Required)'
+      },
+      {
+        type: 'input',
+        name: 'description',
+        message: 'Provide a description of the project (Required)'
+      },
+      {
+        type: 'checkbox',
+        name: 'languages',
+        message: 'What did you build this project with? (Check all that apply)',
+        choices: ['JavaScript', 'HTML', 'CSS', 'ES6', 'jQuery', 'Bootstrap', 'Node']
+      },
+      {
+        type: 'input',
+        name: 'link',
+        message: 'Enter the GitHub link to your project. (Required)'
+      },
+      {
+        type: 'confirm',
+        name: 'feature',
+        message: 'Would you like to feature this project?',
+        default: false
+      },
+      {
+        type: 'confirm',
+        name: 'confirmAddProject',
+        message: 'Would you like to enter another project?',
+        default: false
+      }
     ])
-    .then(answers => console.log(answers));
-    
-// const fs = require('fs');
-// const generatePage = require('./src/page-template');
+    .then(projectData => {
+      portfolioData.projects.push(projectData);
+      if (projectData.confirmAddProject) {
+        return promptProject(portfolioData);
+      } else {
+        return portfolioData;
+      }
+    });
+};
 
-// const pageHTML = generatePage(name, github);
-
-// fs.writeFile('./index.html', pageHTML, err => {
-//   if (err) throw err;
-
-//   console.log('Portfolio complete! Check out index.html to see the output!');
-// });
+promptUser()
+  .then(promptProject)
+  .then(portfolioData => {
+    console.log(portfolioData);
+    // will be uncommented in lesson 4
+    // const pageHTML = generatePage(portfolioData);
+    // fs.writeFile('./index.html', pageHTML, err => {
+    //   if (err) throw new Error(err);
+    //   console.log('Page created! Check out index.html in this directory to see it!');
+    // });
+  });
